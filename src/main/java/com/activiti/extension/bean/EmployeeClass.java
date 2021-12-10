9f8b253f0600b46f4d03649c9b2477393ba825ad
klissda <klisada.po@bcircle.co.th>
2021-12-10 10:48:35 +0700

    commit edit class

--- src/main/java/com/activiti/extension/bean/EmployeeClass.java
+++ src/main/java/com/activiti/extension/bean/EmployeeClass.java
@@ -11,14 +11,17 @@ import org.springframework.stereotype.Component;
 public class EmployeeClass implements JavaDelegate {
     private final Logger logger = LoggerFactory.getLogger(EmployeeClass.class);
     public void execute(DelegateExecution execution) throws Exception {
+
         this.logger.info("[Process=" + execution.getProcessInstanceId() + "][Spring Java Delegate=" + this + "]");
+
         try {
             execution.setVariable("apiSuccess", false);
             String docNo = (String) execution.getVariable("docNo");
             this.logger.info("[docNo] value : " + docNo);
             execution.setVariable("apiSuccess", true);
+
         } catch (Exception var3) {
-            this.logger.error("[H01ASAPDelegate] failed", var3);
+            this.logger.error("[getEmployeeData] failed", var3);
         }
     }
 }
